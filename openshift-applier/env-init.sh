7368db738e2131a513c043c8964a81b2b7a56bd3
Martin Eggen <meggen@redhat.com>
2018-08-31 11:44:11 +0300

    Install python-requests
    Install requests module required by filter-plugins in openshift-applier role

--- openshift-applier/env-init.sh
+++ openshift-applier/env-init.sh
@@ -1,4 +1,5 @@
 ssh root@host01 "yum install epel-release -y"
+ssh root@host01 "yum install python-requests -y"
 ssh root@host01 "yum install ansible -y"
 ssh root@host01 "oc login -u system:admin"
 ssh root@host01 "oc create -f https://raw.githubusercontent.com/pcarney8/rh-applier-quickstart/master/templates/app/ruby.yml -n openshift"
